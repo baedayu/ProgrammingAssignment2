66aa4d9e502455345873e05271c9a7a9a878b659
baeda <baedaenthu@gmail.com>
2014-07-25 21:03:16 +0900

    The assignment2 of coursera course.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Like the instructions, using makeCacheMatrix function, inv stores the inversed matrix
+## cacheSolve looks if an inv has already existing data, if not inverses the matrix
 
-## Write a short comment describing this function
+## Set the value of the matrix
+## Get the value of the matrix
+## Compute the inverse of a square matrix
+## Get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) inv <<-solve
+        getsolve <- function() inv 
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+}        
 
-}
-
-
-## Write a short comment describing this function
+## If inv is not null, then get the cached data
+## If inv is null, then inverse the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       inv <- x$getsolve()
+       if(!is.null(inv)){
+               message("getting cached data")
+               return(inv)               
+       }
+       data <- x$get()
+       inv <- solve(data)
+       x$setsolve(inv)
+       inv
 }
